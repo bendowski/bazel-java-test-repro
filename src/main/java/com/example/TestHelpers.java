9b190ec567536d308abc783bf6d127453668015f
Michal Bendowski <bendowski@google.com>
2016-11-29 12:42:47 +0000

    Reformat code

--- src/main/java/com/example/TestHelpers.java
+++ src/main/java/com/example/TestHelpers.java
@@ -3,8 +3,8 @@ package com.example;
 import org.junit.Assert;
 
 public class TestHelpers {
-    public static void useJunit412() {
-        // This was added in 4.12.
-        Assert.assertArrayEquals(new boolean[]{true}, new boolean[]{true});
-    }
+  public static void useJunit412() {
+    // This was added in 4.12.
+    Assert.assertArrayEquals(new boolean[] {true}, new boolean[] {true});
+  }
 }
--- src/test/java/com/example/UseJunit412.java
+++ src/test/java/com/example/UseJunit412.java
@@ -1,23 +1,24 @@
 package com.example;
 
+import java.net.URL;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.net.URL;
-
 public class UseJunit412 {
-    @Test
-    public void junit412Features() {
-        // This crashes when ran with the :test task.
-        TestHelpers.useJunit412();
-    }
+  @Test
+  public void junit412Features() {
+    // This crashes when ran with the :test task.
+    TestHelpers.useJunit412();
+  }
 
-    @Test
-    public void classPath() throws Exception {
-        URL classFile =
-                Assert.class.getClassLoader().getResource(Assert.class.getName().replace('.', '/') + ".class");
-        Assert.assertFalse(
-                "JUnit classes read from " + classFile.toString(),
-                classFile.toString().contains("TestRunner_deploy.jar"));
-    }
+  @Test
+  public void classPath() throws Exception {
+    URL classFile =
+        Assert.class
+            .getClassLoader()
+            .getResource(Assert.class.getName().replace('.', '/') + ".class");
+    Assert.assertFalse(
+        "JUnit classes read from " + classFile.toString(),
+        classFile.toString().contains("TestRunner_deploy.jar"));
+  }
 }
